3235883851e738e542bdcbfba46ac84876fa6ffc
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-18 00:01:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
-def reverse_each_word
-  x = ["Hello there, and how are you?", "Hi again, just making sure it's reversed!"]
-  x.each
-  p x
+def reverse_each_word(string)
+  string = string.split('')
+  reversed_string = []
+
+  string.each {|word| reversed_string.unshift(word)}
+  p reversed_string.join('')
 end
