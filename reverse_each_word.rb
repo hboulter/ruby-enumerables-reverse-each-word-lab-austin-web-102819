94fcd3131727eb4f0446622841443cdfc961c79b
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-18 00:33:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   sentence = sentence.split('')
   reversed_sentence = []
 
-  sentence.each {|word| reversed_sentence.reverse(word)}
+  sentence.each {reversed_sentence.reverse}
   p reversed_sentence.join('')
 end
